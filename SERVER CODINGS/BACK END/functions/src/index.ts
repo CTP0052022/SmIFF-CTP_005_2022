5cdd8fe843e56752eeaf1a707f1ce67da291c99e
Peterskill <104547222+Peterskill@users.noreply.github.com>
2022-09-14 23:27:13 +0530

    Update index.ts
    Co-Authored-By: Pathuman <91985632+Pathuman-S@users.noreply.github.com>
    Co-Authored-By: n1nura <104968298+n1nura@users.noreply.github.com>

--- SERVER CODINGS/BACK END/functions/src/index.ts
+++ SERVER CODINGS/BACK END/functions/src/index.ts
@@ -1,13 +1,12 @@
-import { firestore } from "firebase-admin";
+import {firestore} from "firebase-admin";
 import * as functions from "firebase-functions";
 
 
 exports.createUser = functions.auth.user().onCreate((user) => {
-    firestore().collection('petrolstation').doc(user.uid).set({
-        Name: user.displayName,
-        Email: user.email,
-        PetrolTankIOT:firestore().collection('petrolstation').doc().id,
-        PetrolWaterflowIOT:firestore().collection('petrolstation').doc().id
-
-    });
+  firestore().collection("petrolstation").doc(user.uid).set({
+    Name: user.displayName,
+    Email: user.email,
+    PetrolTankIOT: firestore().collection("petrolstation").doc().id,
+    PetrolWaterflowIOT: firestore().collection("petrolstation").doc().id,
+  });
 });
