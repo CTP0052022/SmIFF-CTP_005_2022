3b38748dde6d1234031cab2229d7e551fbc15535
n1nura <104968298+n1nura@users.noreply.github.com>
2022-11-12 10:14:50 +0530

    Base for petrol admin
    Co-Authored-By: Pathuman <91985632+Pathuman-S@users.noreply.github.com>
    Co-Authored-By: Peterskill <104547222+Peterskill@users.noreply.github.com>

--- SERVER CODINGS/FRONT END/smiff/src/app/app-routing.module.ts
+++ SERVER CODINGS/FRONT END/smiff/src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { AdminallComponent } from './adminall/adminall.component';
 import {  } from '@angular/fire/auth';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdminpetrolComponent } from './adminpetrol/adminpetrol.component';
 
 
 
 const routes: Routes = [
   {path: 'Adminall',component:AdminallComponent },
+  {path: 'adminall',component:AdminallComponent },
   {path: 'Login',component:LoginComponent },
-  {path: 'Dashboard',component:DashboardComponent}
+  {path: 'login',component:LoginComponent },
+  {path: 'Dashboard',component:DashboardComponent},
+  {path: 'dashboard',component:DashboardComponent},
+  {path: 'Adminpetrol',component:AdminpetrolComponent},
+  {path: 'adminpetrol',component:AdminpetrolComponent}
 ];
 
 @NgModule({
